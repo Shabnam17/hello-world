c15f66d935caa4dc1ea2a97a068281581bb81c93
Shabz17 <shabnamsidhik17@gmail.com>
2017-02-08 16:11:16 +0530

    Update trial.js

--- trial.js
+++ trial.js
@@ -1,24 +1,35 @@
 var trialApp = angular.module("someApp",[]);
 
-trialApp.controller("SomeController", function($scope, $timeout,$interval,$location){
+trialApp.controller("SomeController", function($scope, $timeout,$interval,$location,$http){
 $scope.myHeading ="Happy Christmas";
 $scope.parameter =new Date().toLocaleTimeString();
-$timeout(function(){
-$scope.myHeading ="Happy New Year";
-}, 1000);
+  $timeout(function(){
+  $scope.myHeading ="Happy New Year";
+  }, 1000);
 
 
-$interval(function(){
-$scope.parameter =new Date().toLocaleTimeString();
-}, 2000);
+  $interval(function(){
+  $scope.parameter =new Date().toLocaleTimeString();
+  }, 2000);
+
+
 $scope.myLocation = $location.absUrl();
 
-$scope.call = function(){
-     console.log(req.url);
-};
-
 $scope.mydata ={val :'jake'};
-$scope.$watch('mydata.val' ,function(newval){
+$scope.$watch('mydata.val' , function(newval){
 $scope.mydata.toolong =newval.length>15;
 });
+
+
+$http({
+  method: 'GET',
+  url: 'https://api.github.com/users/pedrozath/repos'
+}).then(function successCallback(response) {
+
+ $scope.resName = response.data[0].name;
+ $scope.x = response.data;
+}, function errorCallback(response) {
+    console.log("errorCallback");
+});
+
 });
